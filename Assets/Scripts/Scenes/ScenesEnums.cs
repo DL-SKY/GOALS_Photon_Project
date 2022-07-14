7c7b72a1b8e602c63dcb1503aaaea97fae257653
Alexander Danilovsky <alex.dllsky@gmail.com>
2022-07-14 20:45:29 +0300

    NavMeshComponentsMaster

--- Assets/Scripts/Scenes/ScenesEnums.cs
+++ Assets/Scripts/Scenes/ScenesEnums.cs
@@ -7,5 +7,10 @@
         Main = 0,
 
         MainMenu = 1,
+
+        //...
+
+        Test01 = 101,
+        Test02 = 102,
     }
 }
---
+++ Assets/Scripts/Test/TestJSON.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GOALS.Test
+{
+    public class MyTestClass
+    {
+        public int index;
+        public float range;
+        public bool isDead;
+        public List<float> points;
+        public Dictionary<string, int> dic;
+    }
+
+    public class TestJSON : MonoBehaviour
+    {
+        private void Start()
+        {
+            var test = new MyTestClass()
+            {
+                index = 999,
+                range = 2.5f,
+                isDead = false,
+                points = new List<float> { 0.5f, 1.5f, 3.5f, },
+                dic = new Dictionary<string, int>
+                {
+                    { "one", 1 },
+                    { "five", 5 },
+                }
+            };
+
+            var json = JsonConvert.SerializeObject(test);
+            Debug.LogError(json);
+
+            test = JsonConvert.DeserializeObject<MyTestClass>("{\"index\":123,\"range\":0.5,\"isDead\":true," +
+                "\"points\":[10.0,15.75],\"dic\":{\"one-2\":12,\"five-2\":52}}");
+            json = JsonConvert.SerializeObject(test);
+            Debug.LogError(json);
+        }
+    }
+}
